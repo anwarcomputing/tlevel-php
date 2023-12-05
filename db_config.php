07f0309f0d6c6ce286b040e5f0af3660159a5da3
anwarcomputing <152861780+anwarcomputing@users.noreply.github.com>
2023-12-05 00:18:11 +0000

    DatabaseConfig

---
+++ db_config.php
@@ -0,0 +1,15 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "my_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
